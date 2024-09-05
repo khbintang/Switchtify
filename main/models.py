c0f8106f5d1fe1349865cf7a7743f8b8d35669ca
Khairul Bintang <khairulbintang08@gmail.com>
2024-09-05 16:25:42 +0700

    Membuat sebuah routing pada urls.py aplikasi main untuk memetakan fungsi yang telah dibuat pada views.py.

--- main/models.py
+++ main/models.py
@@ -1,15 +1,15 @@
 # views.py
 from django.shortcuts import render
-from .models import MoodEntry  
+
 
 def product_list(request):
-    products = MoodEntry.objects.all()  
+    
     
     context = {
-        'products': products,
+        
         'name' : 'gateron linear fizzy oil keyboard switch',
         'price': '8000',         
         'description': 'Faculty of Computer Science'  
     }
     
-    return render(request, 'product_list.html', context)
+    return render(request, 'main.html', context)
