fb3c84bc7611f77041560815849c6f82c6a64ffa
Khairul Bintang <khairulbintang08@gmail.com>
2024-09-10 04:39:21 +0700

    Mendeploy lagi ke pws

--- main/views.py
+++ main/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name' : 'Gateron Linear Fizzy Oil ',
-        'price': '8.000 / piece',
-        'description': 'A linear profile keyboard switch with a smooth typing feel and loud clack'
+        'name' : 'Gateron Linear Fizzy Oil Keyboard Switch ',
+        'price': 'Rp 8.000 / piece',
+        'type' : ' Linear',
+        'sound_profile' : 'Loud clacky, almost marbly',
+        'lube': 'Factory lubed'
     }
 
     return render(request, "main.html", context)
