054599cdececfe9a308bf5db1f85bf894330facf
Khairul Bintang <khairulbintang08@gmail.com>
2024-09-11 10:28:43 +0700

    Menambahkan nama dan kelas pada project lagi

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product
 
+# Product detail view
 def product_detail(request):
     product = Product.objects.first()  # Get the product object, replace with your logic
     context = {
@@ -10,5 +11,14 @@ def product_detail(request):
         'type': product.type,
         'sound_profile': product.sound_profile,
         'image_url': product.image.url,  # assuming image field exists
+    }
+    return render(request, 'main.html', context)
+
+# About app view
+def about_app(request):
+    context = {
+        'app_name': 'Switchtify',  # Nama aplikasimu
+        'student_name': 'Khairul Bintang',
+        'class_name': 'PBP F',
     }
     return render(request, 'main.html', context)
